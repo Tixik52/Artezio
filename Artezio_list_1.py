b06ba5016cb7a663773b6a22b63d99b0cc793eb3
Tixik52 <potapov.anatoly@gmail.com>
2015-03-18 15:55:26 +0300

    list comprehension
    update

--- Artezio_list_1.py
+++ Artezio_list_1.py
@@ -12,12 +12,7 @@ def func_list_c(lst):
 		lst2.append(x**2)
 	return lst2
 	
-def func_list_c2(lst):
-	return [x for x in lst[1::2] if x is list[1::2]]
 
-if __name__ == '__main__':
-	
-	print(func_list_a([1,2,3,4,5,6,7,8,9,10]))
-	print(func_list_b([1,2,3,4,5,6,7,8,9,10]))
-	print(func_list_c([1,2,3,4,5,6,7,8,9,10]))
-	print(func_list_c2([1,2,3,4,5,6,7,8,9,10]))
+print(func_list_a([1,2,3,4,5,6,7,8,9,10]))
+print(func_list_b([1,2,3,4,5,6,7,8,9,10]))
+print(func_list_c([1,2,3,4,5,6,7,8,9,10]))
