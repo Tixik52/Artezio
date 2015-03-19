9c19f3469b1d92c7930c8448f2dbe4b31956a279
Tixik52 <potapov.anatoly@gmail.com>
2015-03-20 00:19:34 +0300

    Function ZIP
    Результат изменения функции my_zip_method_count(*args) находится в
    строке 7 в скобках range()

--- Artezio_my_zip_function.py
+++ Artezio_my_zip_function.py
@@ -1,26 +1,19 @@
 
 def my_zip_function(*obj):
 	if not(obj):
-		print("Not args", "\n")
+		print("No args", "\n")
 		return None
 	else:
-		n = my_zip_method_count(obj)
-		for number_elem in range(n):
+		for number_elem in range(min(len(x) for x in obj)):
 			result = []
 			for number_obj in obj:
 				result.append(number_obj[number_elem])
 			yield (tuple(result))
 
-def my_zip_method_count(*args):
-	number = []
-	for x in args:
-		i=0
-		for k in x: i = i + 1
-		number.append(i)
-	return min(number)
+
 
 lst = [1,2,3,4,5,6,7,8,9,10]
-i = my_zip_function(range(5), ['a','b','c','d'], 'qweasd', lst)
+i = my_zip_function(range(10), ['a','b','c','d','e'], 'qweasdzxc', lst, range(17))
 for o in i:
 	print(o)
 
