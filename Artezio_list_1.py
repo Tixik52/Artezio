e9e59a639e6a5ec6c13eed447ec5c7a63153935f
Tixik52 <potapov.anatoly@gmail.com>
2015-03-19 20:40:27 +0300

    list comprehension
    updated function "func_list_c"

--- Artezio_list_1.py
+++ Artezio_list_1.py
@@ -6,11 +6,7 @@ def func_list_b(lst):
 	return [x for x in lst[1::2]]
 		
 def func_list_c(lst):
-	lst2=[]
-	for x in lst[1::2]:
-		lst2.append(x)
-		lst2.append(x**2)
-	return lst2
+	return [x**2 for x in lst[1::2] if x != 0 and x%2 == 0]
 	
 
 print(func_list_a([1,2,3,4,5,6,7,8,9,10]))
